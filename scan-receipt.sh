d798fe9c6597d8fd36835f592e5b1c0861fe811b
Mathis <sveitser@gmail.com>
2012-08-09 17:18:57 +0800

    PDF output

--- scan-receipt.sh
+++ scan-receipt.sh
@@ -1,14 +1,21 @@
 #!/bin/sh
-# usage: scan-receipt.sh identifier
+# usage: scan-receipt.sh identifier [date] 
+# output YY_MM_DD_identifier.pdf resp. date_identifier.pdf
 #
 
 # find scanner with "scanimage -L"
 DEVICE="hpaio:/usb/Photosmart_C4400_series?serial=MY96HR12KW0557"
 
-# scan image
-scanimage -d $DEVICE --mode Lineart --resolution 300  > test.pnm
+if [ -z $2 ]; then DATE=`date +%y_%m_%d`; else DATE=$2; fi
 
-# crop image
+FNAME=${DATE}_${1}.pdf
+
+if [ -f $FNAME ]; then echo "File $FNAME exists. Change the name."; exit 1; fi
+
+# scan image
+scanimage -d $DEVICE --mode Lineart --resolution 300 --format=tiff > tmp.tiff
 
 # convert image
+tiff2pdf tmp.tiff > $FNAME
 
+rm -rf tmp.tiff
